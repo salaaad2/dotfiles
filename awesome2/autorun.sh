513f887c60b058e51ac114f4a7edaf408fb4c9af
salaaad2 <arthurdurant263@gmail.com>
2021-11-05 13:35:18 +0100

    uptodate awesome

---
+++ awesome2/autorun.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function run {
+	if ! pgrep -f $1 ;
+	then 
+		$@&
+	fi
+}
+
+run compton
+run xset r rate 200 150
+run setxkbmap -layout "us,fr" -option "grp:alt_shift_toggle"
+run setwp
